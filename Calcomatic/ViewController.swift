57d7b66237ebb61493a095f572116a75d42a7daa
Rovshan Rasulov <soulofchinder.2@gmail.com>
2025-03-20 14:44:07 +0400

    Initial Commit

---
+++ Calcomatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calcomatic
+//
+//  Created by Rovshan Rasulov on 20.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
